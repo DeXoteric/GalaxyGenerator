2e1e167e3430d5360948300e3e58dccb821c7783
DeXoteric <marlev@poczta.pl>
2018-11-25 11:36:26 +0100

    SPC Part 2

--- GalaxyGenerator/Assets/Scripts/Galaxy.cs
+++ GalaxyGenerator/Assets/Scripts/Galaxy.cs
@@ -1,22 +1,50 @@
 ﻿using UnityEngine;
 
-public class Galaxy : MonoBehaviour
+namespace GalaxyGenerator
 {
-    [SerializeField] private int numberOfStars = 300;
-    [SerializeField] private int maximumRadius = 100;
-
-    private void Start()
+    public class Galaxy : MonoBehaviour
     {
-        for (int i = 0; i < numberOfStars; i++)
+        [SerializeField] private int numberOfStars = 300;
+        [SerializeField] [Range(0, 50)] private int minimumRadius = 0;
+        [SerializeField] [Range(70, 100)] private int maximumRadius = 100;
+        [SerializeField] private float minDistBetweenStars = 2f;
+
+        private void Start()
         {
-           GameObject start=  GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            int failCount = 0;
 
-            float distance = Random.Range(0, maximumRadius);
-            float angle = Random.Range(0, 2 * Mathf.PI);
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                Star starData = new Star("Star " + i, Random.Range(2, 7));
+                Debug.Log("Created " + starData.starName + " with " + starData.numberOfPlanets + " planets");
 
-            Vector3 position = new Vector3(distance * Mathf.Cos(angle), distance * Mathf.Sin(angle), 0);
+                float distance = Random.Range(minimumRadius, maximumRadius);
+                float angle = Random.Range(0, 2 * Mathf.PI);
 
-            start.transform.position = position;
+                Vector3 position = new Vector3(distance * Mathf.Cos(angle), distance * Mathf.Sin(angle), 0);
+
+                Collider[] positionCollider = Physics.OverlapSphere(position, minDistBetweenStars);
+
+                if (positionCollider.Length == 0)
+                {
+                    GameObject star = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    star.name = starData.starName;
+                    star.transform.position = position;
+
+                    failCount = 0;
+                }
+                else
+                {
+                    i--;
+                    failCount++;
+                }
+
+                if (failCount > numberOfStars)
+                {
+                    Debug.LogError("Could not fit all stars in the galaxy. Consider smaller distance between stars!");
+                    break;
+                }
+            }
         }
     }
 }
---
+++ GalaxyGenerator/Assets/Scripts/Star.cs
@@ -0,0 +1,14 @@
+﻿namespace GalaxyGenerator
+{
+    public class Star
+    {
+        public string starName { get; protected set; }
+        public int numberOfPlanets { get; protected set; }
+
+        public Star(string starName, int numberOfPlanets)
+        {
+            this.starName = starName;
+            this.numberOfPlanets = numberOfPlanets;
+        }
+    }
+}
